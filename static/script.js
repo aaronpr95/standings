bb8d596f137d19bd37d27eaefae542e3331b71bf
aaronpr95 <aaronp.morales@gmail.com>
2024-11-08 10:49:42 +0000

    Deploying to gh-page from @ aaronpr95/standings@fab320e97eb750b4f3d4c8396eda51413eb048e4 🚀

---
+++ static/script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the elements for the toggle button and the table
+    const toggleViewLink = document.getElementById('toggleView');
+    const table = document.querySelector('table');
+    const hiddenCells = document.querySelectorAll('td.hidden');
+    const hiddenHeaders = document.querySelectorAll('th.hidden');
+
+    // Add an event listener for the toggle button
+    toggleViewLink.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the default link behavior
+
+        // Toggle between the compact and full modes by adding/removing classes
+        table.classList.toggle('short-mode');
+        table.classList.toggle('full-mode');
+
+        // Toggle the visibility of hidden cells and headers
+        hiddenCells.forEach(cell => cell.classList.toggle('hidden'));
+        hiddenHeaders.forEach(header => header.classList.toggle('hidden'));
+
+        // Change the link text based on the current mode
+        toggleViewLink.textContent = table.classList.contains('short-mode') 
+            ? 'Ver Detalles'  // If short mode is active, show 'Ver Detalles'
+            : 'Ver Menos';     // If full mode is active, show 'Ver Menos'
+    });
+});
